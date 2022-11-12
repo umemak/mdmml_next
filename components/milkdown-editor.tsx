16f364a516ed1647d529ab059d5810c100c10399
umemak <umematsu@gmail.com>
2022-11-12 10:25:35 +0000

    add packages

---
+++ components/milkdown-editor.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Editor, rootCtx } from '@milkdown/core';
+import { nord } from '@milkdown/theme-nord';
+import { ReactEditor, useEditor } from '@milkdown/react';
+import { commonmark } from '@milkdown/preset-commonmark';
+
+export const MilkdownEditor: React.FC = () => {
+    const { editor } = useEditor((root) =>
+        Editor.make()
+            .config((ctx) => {
+                ctx.set(rootCtx, root);
+            })
+            .use(nord)
+            .use(commonmark),
+    );
+
+    return <ReactEditor editor={editor} />;
+};
