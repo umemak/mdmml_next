c7c63bb86f90df904e3fbde9daa7ff7dd12eaa26
umemak <umematsu@gmail.com>
2022-12-24 15:23:59 +0900

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { MilkdownEditor } from '../components/milkdown-editor'
 import styles from '../styles/Home.module.css'
 import MidiPlayer from 'react-midi-player'
 
-var _data=atob('\
+const _data = Buffer.from('\
 TVRoZAAAAAYAAQADAGRNVHJrAAAAGgD/AwtMaXR0bGUgTGFtZQD/UQMKLCsA/y8ATVRyawAAAPMA/wMG\
 THlyaWNzAP8BGEBUTWFyeSBXYXMgQSBMaXR0bGUgTGFtZWT/AQNcTWFL/wEDcnkgGf8BBHdhcyAy/wEC\
 YSAy/wEDbGl0Mv8BBHRsZSAy/wEFbGFtZSxk/wEEL0xpdDL/AQR0bGUgMv8BBWxhbWUsZP8BBC9MaXQy\
@@ -14,7 +14,7 @@ VHJrAAAA8gD/AwVNdXNpYwDAC2SQQH9LgEBAAJA+fxmAPkAAkDx/MoA8QACQPn8ygD5AAJBAfzKAQEAA
 kEB/MoBAQACQQH9agEBACpA+fzKAPkAAkD5/MoA+QACQPn9agD5ACpBAfzKAQEAAkEN/MoBDQACQQ39a\
 gENACpBAf0uAQEAAkD5/GYA+QACQPH8ygDxAAJA+fzKAPkAAkEB/MoBAQACQQH8ygEBAAJBAfzKAQEAZ\
 kEB/GYBAQACQPn8ygD5AAJA+fzKAPkAAkEB/MoBAQACQPn8ygD5AAJA8f2RAZABDZABIf1qAPEAAQEAA\
-Q0AASEAK/y8A');
+Q0AASEAK/y8A', 'base64')
 
 export default function Home() {
   return (
